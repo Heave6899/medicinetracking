9a755150f548523c354a093c4b907cb648d16eb4
Heave6899 <vanshfpatel@outlook.com>
2019-09-15 15:49:42 +0530

    v1.2.3

--- contracts/Medicine.sol
+++ contracts/Medicine.sol
@@ -9,56 +9,48 @@ contract Medicine {
         string name;
         string expdate;
         string date;
-    }
+  }
   mapping(uint => Medicines) public medicines;
-  //mapping(address=>bool) public medicalorg;
   uint public count;
+  bool flag;
   function addMedicine(string memory _mfg,string memory _name, string memory _expdate,string memory _date) public {
         count ++;
-        medicines[count] = Medicines(count,_mfg, _name, _expdate, _date);
-    }
-    constructor () public {
-        addMedicine("Manufacturer 1","Medicine 1","2019-02-28","2017-03-20");
-        addMedicine("Manufacturer 2","Medicine 2","2020-05-04","2016-02-20");
-    }
-  /*function set(string memory _h,string memory _date) public{
-    h = _h;
-    date = _date;
-  }*/
-  function getname(uint i) public view returns(string memory){
-    return medicines[i].name;
-  }
-  function getdate(uint i) public view returns(string memory){
-    return medicines[i].date;
-  }
-  function bytes32ToString (bytes32 data) public pure returns (string memory) {
-    bytes memory bytesString = new bytes(32);
-    for (uint j = 0;j < 32;j++) {
-        byte char = byte(bytes32(uint(data) * 2 ** (8 * j)));
-        if (char != 0) {
-            bytesString[j] = char;
-        }
-    }
-    return string(bytesString);
-}
-  function getexpdate(uint i) public view returns(string memory){
-    return medicines[i].expdate;
-  }
-  function searchexpdate(string memory _name) public view returns(string memory){
-    for(uint i = 1; i <= count; i++)
-    {
-      if(keccak256(abi.encodePacked((medicines[i].name))) == keccak256(abi.encodePacked((_name)))){
-        return medicines[i].expdate;
+        for(uint i = 0;i<=count;i++){
+            flag = false;
+            for(uint j = 0;j<=count;j++){
+                if((keccak256(abi.encodePacked((_name))) == keccak256(abi.encodePacked((medicines[j].name))))){
+                medicines[count] = Medicines(count,_mfg, _name, medicines[j].expdate, medicines[j].date);
+                flag = true;
+                    break;
+                }
+            }
+          }
+          if(flag==false)
+          medicines[count] = Medicines(count,_mfg, _name, _expdate, _date);
+      }
+  constructor () public {
+        addMedicine("Manufacturer 1-M","Medicine 1-1","2019-02-28","2017-03-20");
+        addMedicine("Manufacturer 2-M","Medicine 2-2","2020-05-04","2016-02-20");
+        addMedicine("Manufacturer 3-M","Medicine 1-1","2019-02-25","2017-03-19");
+        addMedicine("Manufacturer 4-C","Medicine 1-1","2019-02-25","2017-03-19");
 
-      }
+
     }
-  }
-  function searchdate(string memory _name) public view returns(string memory){
-    for(uint i = 1; i <= count; i++)
-    {
-      if(keccak256(abi.encodePacked((medicines[i].name))) == keccak256(abi.encodePacked((_name)))){
-        return medicines[i].date;
-      }
-    }
-  }
+  //function getname(uint i) public view returns(string memory){
+  //  return medicines[i].name;
+  //}
+  //function getdate(uint i) public view returns(string memory){
+  //  return medicines[i].date;
+  //}
+  //function getexpdate(uint i) public view returns(string memory){
+  //  return medicines[i].expdate;
+  //}
+  //function search(string memory _name) public view returns(bool){
+  //  for(uint i = 0; i <= count; i++)
+  //  {
+  //    if((keccak256(abi.encodePacked((medicines[i].name))) == keccak256(abi.encodePacked((_name))))){
+  //          return true;
+  //    }
+  // }
+  //}
 }
--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ App = {
 
   init: function(){
     $("#content").hide();
+    $("#content-cli").hide();
     return App.initWeb3();
   },
 
@@ -25,6 +26,7 @@ App = {
       if (err === null) {
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
+        $("#accountAddressclient").html("Your Account: " + account);
       }
     });
     $.getJSON("Medicine.json", function(medicine) {
@@ -37,13 +39,17 @@ App = {
       if (err === null) {
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
+        $("#accountAddressclient").html("Your Account: " + account);
       }
     });
     var name = $('#medname').val();
-    var mfg = $('#manufacturer').val();
+    var mfg1 = $('#manufacturer').val();
     var date = $('#meddate').val();
     var expdate = $('#medexpdate').val();
-    //console.log(name+" "+date);
+    var uid = $('#meduid').val();
+    var name = name + "-" + uid;
+    var mfg = mfg1 + "-M";
+    console.log(name+" "+date);
     if(date>expdate){
       alert("Invalid Expiry Date, cannot be less than manufacturing date");
       $("#formadd").trigger("reset");
@@ -55,14 +61,47 @@ App = {
     $("#formadd").trigger("reset");
     console.log(datestr);
     App.contracts.Medicine.deployed().then(function(instance){
-      return instance.addMedicine(mfg,namestr, expdatestr, datestr, {from: App.account, gas: 2000000 });
+      return instance.addMedicine(mfg,namestr, expdatestr, datestr, {from: App.account, gas: 2500000 });
+    }).catch(function(err){
+      console.error(err);
+    });
+  }
+  },
+
+  addClient:function(){
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddressclient").html("Your Account: " + account);
+      }
+    });
+    var name = $('#mednamec').val();
+    var mfg1 = $('#client').val();
+    var date = $('#meddatec').val();
+    var expdate = $('#medexpdatec').val();
+    var uid = $('#meduidc').val();
+    var name = name + "-" + uid;
+    var mfg = mfg1 + "-C";
+    //console.log(name+" "+date);
+    if(date>expdate){
+      alert("Invalid Expiry Date, cannot be less than manufacturing date");
+      $("#formaddc").trigger("reset");
+    }
+    else{
+    var namestr = name.toString();
+    var datestr = date.toString();
+    var expdatestr = expdate.toString();
+    $("#formaddc").trigger("reset");
+    //console.log(datestr);
+    App.contracts.Medicine.deployed().then(function(instance){
+      return instance.addMedicine(mfg,namestr, expdatestr, datestr, {from: App.account, gas: 2500000 });
     }).catch(function(err){
       console.error(err);
     });
   }
   },
   
-  search: function(){
+  searchmfg: function(){
     var instance;
     var med = $("medicines");
     med.empty();
@@ -76,20 +115,71 @@ App = {
       {
         instance.medicines(i).then(function(medicine){
           var mfg = medicine[1];
-          var name = medicine[2];
+          var name1 = medicine[2];
           var expdate = medicine[3];
+          var date = medicine[4];
+          var uidp = name1.split('-');
+          var uid = uidp[1];
+          var name = uidp[0];
+          var mp = mfg.split('-');
+          var m = mp[1];
+          var mfg = mp[0];
           var today = new Date();
           var parts = expdate.split('-');
           var mydate = new Date(parts[0], parts[1] - 1, parts[2]);
           var date = medicine[4];
           var expiry = "No";
           if(mydate<today){expiry="Yes";}
-          var medtemplate = "<tr><th>" + mfg + "</th><td>" + name + "</td><td>" + date + "</td><td>" + expdate + "</td><td>" + expiry + "</td></tr>"
-          console.log(name);
+          var medtemplate = "<tr><th>" +uid+ "</th><td>"+ mfg + "</td><td>" + name + "</td><td>" + date + "</td><td>" + expdate + "</td><td>" + expiry + "</td></tr>"
+          //console.log(name);
           if(search == name){
             $("#content").show();
+            if(m!='C'){
             $("#medicinedetails").append(medtemplate);
-          }
+            }
+        }
+        });
+      }
+    })
+  },
+
+  searchclient: function(){
+    var instance;
+    var med = $("medicines");
+    med.empty();
+    var search = $('#searchbarcli').val();
+    App.contracts.Medicine.deployed().then(function(_instance){
+    instance =_instance;
+    return instance.count();
+    }).then(function(count){
+      $("#medicinedetailsc").html("");
+      for(var i=0;i<=count;i++)
+      {
+        instance.medicines(i).then(function(medicine){
+          var mfg = medicine[1];
+          var name1 = medicine[2];
+          var expdate = medicine[3];
+          var date = medicine[4];
+          var uidp = name1.split('-');
+          var uid = uidp[1];
+          var name = uidp[0];
+          var mp = mfg.split('-');
+          var m = mp[1];
+          var mfg = mp[0];
+          var today = new Date();
+          var parts = expdate.split('-');
+          var mydate = new Date(parts[0], parts[1] - 1, parts[2]);
+          var date = medicine[4];
+          var expiry = "No";
+          if(mydate<today){expiry="Yes";}
+          var medtemplate = "<tr><th>" +uid+ "</th><td>"+ mfg + "</td><td>" + name + "</td><td>" + date + "</td><td>" + expdate + "</td><td>" + expiry + "</td></tr>"
+          console.log(name);
+          if(search == name){
+            $("#content-cli").show();
+            if(m!='M'){
+            $("#medicinedetailsc").append(medtemplate);
+            }
+        }
         });
       }
     })
