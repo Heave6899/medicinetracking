396cb9400f480fae92cf6e4161df4c3f9f5c8762
Tanuj Gupta <vanshfpatel@outlook.com>
2020-04-08 01:38:05 +0530

    Start ignoring

--- secret.js
+++
@@ -1,5 +0,0 @@
-var infuraKey = "96fa1c881e2d49ad8673a053d2cd9f2c";
-var accountPK = "A3E2CD84C5C734F3979F92E30B584D9D56CCF9A584FB9ADCDF9E9CE6242CB361";
-var mainnetPK = accountPK;
-var ropstenPK = accountPK;
-module.exports = {infuraKey: infuraKey, mainnetPK: mainnetPK, ropstenPK:ropstenPK};
